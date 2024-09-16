772513b528789a410de672223dcefcefe7be4cbc
Arthur <aduarte98@outlook.com>
2024-09-16 16:13:39 -0300

    Alteração 16/09

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,9 @@
 import VideoComponent from "../../components/videoComponents";
 import { Container,ContainerVideo, ContainerCategory, Category, LinhaMenu, ContainerShorts, TitleShorts } from "./styles";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../contexts/useContext";
 import ShortsComponent from "../../components/shortsComponents";
-
+import axios from 'axios'
 
 interface Props {
     openMenu: boolean;
@@ -11,6 +11,46 @@ interface Props {
 
 
 function Home(){
+
+    interface Videos {
+        id:string;
+        snippet: {
+            title: string;
+            thumbnails: {
+                high: {
+                    url: string;
+                }
+                maxres: {
+                    url: string;
+                }
+            }
+            channelTitle: string;
+            publishedAt: string;
+        },
+        statistics: {
+            viewCount : string;
+        }
+    }
+
+    const [videos, setVideos] = useState <Videos[]>([]);
+    const {categoryId} = useCategoryContext();
+
+    useEffect(() => {
+        load()
+    }, [categoryId])
+
+    const API_KEY = ''
+
+    const url = ''
+
+    async function load() {
+        try {
+            const resposta = await axios.get(url)
+            setVideos(resposta.data.items)
+        }catch(erro) {
+            console.log(erro)
+        }
+    }
 
     const contexto = useContext(UserContext)
 
@@ -33,7 +73,15 @@ function Home(){
                 ))}
             </ContainerCategory>
             <ContainerVideo openMenu={openMenu}>
-                <VideoComponent />
+                {videos.map((video) => 
+                <VideoComponent 
+                title={video.snippet.title}
+                thumbnail={video.snippet.thumbnails.maxres?.url}
+                channelLImage={video.snippet.channelTitle.charAt(0).toLocaleUpperCase()}
+                channelLName={video.snippet.channelTitle}
+                details={`${formatViewCount(Number(video.statistics.viewCount))}`} //27:41
+                />
+                )}
                 <VideoComponent/>
                 <VideoComponent/>
                 <VideoComponent/>
